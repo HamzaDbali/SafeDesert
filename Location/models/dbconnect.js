1f71f24552cc6a1dec19e58743edc3f554f94be2
hamza <ahmeddbali06@gmail.com>
2026-02-22 13:42:03 +0000

    fix

---
+++ Location/models/dbconnect.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+
+// for connection local : mongoose.connect('mongodb://localhost:27017/yaploud')
+mongoose.connect('mongodb+srv://saharauser:saharauser123@cluster0.hf26jws.mongodb.net/')
+
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error('Database not connected: ' + err);
+  });
+
+module.exports = mongoose;
